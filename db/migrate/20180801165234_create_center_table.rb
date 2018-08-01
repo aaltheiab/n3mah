8269c145d3b0c0cdc641e9624b53c634dfa660de
aaltheiab <aaltheiab@morniksa.com>
2018-08-01 20:26:02 +0300

    [reorder] center attribute

--- db/migrate/20180801165234_create_center_table.rb
+++ db/migrate/20180801165234_create_center_table.rb
@@ -1,12 +1,12 @@
 class CreateCenterTable < ActiveRecord::Migration[5.2]
   def change
     create_table :centers do |t|
-      t.timestamps null: false
       t.float :lat
       t.float :lng
       t.jsonb :name
       t.string :status
       t.integer :capacity
+      t.timestamps null: false
     end
   end
 end
