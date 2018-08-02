6f41d90fa273fca52d7447b74bc3ea223f2c4391
aaltheiab <aaltheiab@morniksa.com>
2018-08-02 14:58:53 +0300

    [validate] capacity presence in node and set its default value.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,16 @@
 class Node < ActiveRecord::Base
   belongs_to :branch
   has_many :orders, as: :owner
+
+  validate :capacity, presence: true
+
+  before_validation :set_capacity
+
+
+  private
+
+  # set default capacity to 100 if not provided
+  def set_capacity
+    self.capacity ||= 100
+  end
 end
