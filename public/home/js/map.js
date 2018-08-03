3520bc5825555f318ad795f20eae2366647e6fc9
aaltheiab <aaltheiab@morniksa.com>
2018-08-03 04:00:03 +0300

    [enhance] style css and order show page. [change] map.js functionality. [remove] details.js (no longer needed)

--- public/home/js/map.js
+++ public/home/js/map.js
@@ -1,28 +1,37 @@
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: new google.maps.LatLng(21.354831,39.983886),
-          mapTypeId: 'terrain'
-        });
+var map;
 
-        // Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        // This example uses a local copy of the GeoJSON stored at
-        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-        script.src = '/../home/js/details.js';
-        document.getElementsByTagName('head')[0].appendChild(script);
-      }
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: new google.maps.LatLng(21.357831, 39.983886),
+        mapTypeId: 'roadmap'
 
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
+    });
+    var iconBase = '/../home/img/';
+    var icons = {
+        pickUp: {
+            icon: iconBase + 'a-pin.png'
+        },
+        dropOff: {
+            icon: iconBase + 'b-pin.png'
         }
-      }
+    };
+
+    var features = [ {
+        position: new google.maps.LatLng(21.357841, 39.971886),
+        type: 'pickUp'
+    }, {
+        position: new google.maps.LatLng(21.354831, 39.983886),
+        type: 'dropOff'
+    }
+    ];
+
+    // Create markers.
+    features.forEach(function(feature) {
+        var marker = new google.maps.Marker({
+            position: feature.position,
+            icon: icons[feature.type].icon,
+            map: map
+        });
+    });
+}
